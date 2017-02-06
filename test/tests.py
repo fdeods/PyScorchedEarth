92ba98fd04322c0d58fe8a490e5930938a635064
robocop3 <zkg12923@dsiay.com>
2017-02-07 00:20:48 +0100

    added tests for Tank class

--- test/tests.py
+++ test/tests.py
@@ -1,6 +1,9 @@
+import os
 import unittest
 import pygame
 from menu.option import Option
+from game_core.tank import Tank
+from game_core.constants import *
 
 
 def empty_function():
@@ -55,6 +58,61 @@ class OptionTestCase(unittest.TestCase):
         isCalled = False
         test.select()
         self.assertEqual(isCalled, True)
+
+
+class TankTestCase(unittest.TestCase):
+
+    os.chdir('..')
+
+    def test_tank_calculate_distance_from_tank_center(self):
+        pygame.init()
+        tank = Tank((display_height, display_width), (100, 100), (200, 200), black)
+        point = tank.calculate_distance_from_tank_center((200, 200))
+        self.assertEquals(point, 141)
+
+    def test_tank_turret_and_coordinates(self):
+        pygame.init()
+        tank = Tank((display_height, display_width), (100, 100), (200, 200), black)
+        tac = tank.get_turret_end_coordinates()
+        self.assertEquals(tac, (0, 0))
+
+    def test_tank_update_turret_angle(self):
+        pygame.init()
+        tank = Tank((display_height, display_width), (100, 100), (200, 200), black)
+        tank.turret_angle = 0
+        self.assertEquals(tank.turret_angle, 0)
+        tank.update_turret_angle(50)
+        self.assertEquals(tank.turret_angle, 1.5707963267948966)
+        tank.turret_angle = 0
+        self.assertEquals(tank.turret_angle, 0)
+        tank.update_turret_angle(-20)
+        self.assertEquals(tank.turret_angle, -1.5707963267948966)
+
+    def test_tank_update_power(self):
+        pygame.init()
+        tank = Tank((display_height, display_width), (100, 100), (200, 200), black)
+        self.assertEquals(tank.tank_power, 50)
+        tank.update_tank_power(200)
+        self.assertEquals(tank.tank_power, 100)
+
+    def test_tank_get_health(self):
+        pygame.init()
+        tank = Tank((display_height, display_width), (100, 100), (200, 200), black)
+        self.assertEquals(tank.tank_health, 100)
+        self.assertEquals(tank.get_tank_health(), 100)
+
+    def test_tank_get_health(self):
+        pygame.init()
+        tank = Tank((display_height, display_width), (100, 100), (200, 200), black)
+        self.assertEquals(tank.position, [100, 100])
+        self.assertEquals(tank.get_tank_position(), (100, 100))
+
+    def test_tank_update_position(self):
+        pygame.init()
+        tank = Tank((display_height, display_width), (100, 100), (200, 200), black)
+        self.assertEquals(tank.position, [100, 100])
+        tank.update_tank_position((200, 200))
+        self.assertEquals(tank.position(), (200, 200))
 
 
 if __name__ == '__main__':
